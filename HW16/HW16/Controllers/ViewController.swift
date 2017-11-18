7c564d353735556e612122f919da5debef18a228
Alex Neo <100frand@gmail.com>
2017-11-18 10:50:22 +0200

    Final HW16

--- HW16/HW16/Controllers/ViewController.swift
+++ HW16/HW16/Controllers/ViewController.swift
@@ -10,47 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet private weak var ibImageView: UIImageView!
-    private var isMovableViewTouch = false
+    @IBOutlet private weak var ibView: UIView!
     
+    private var isMovableViewTouch = false
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        ibImageView.backgroundColor = .orange
+        ibView.backgroundColor = .orange
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(panRecognized(_:)))
+        ibView.addGestureRecognizer(panGesture)
+        
+        let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(pinchGesture(_:)))
+        ibView.addGestureRecognizer(pinchGesture)
+        
+        let rotationGestiure = UIRotationGestureRecognizer(target: self, action: #selector(rotationRecognazed(_:)))
+        ibView.addGestureRecognizer(rotationGestiure)
     }
     
-    @IBAction func panRecognized(_ sender: UIPanGestureRecognizer) {
-        let point = sender.location(in: ibImageView)
-        ibImageView.center = point
-        print(point.x)
+    @objc func panRecognized(_ sender: UIPanGestureRecognizer) {
+        let position = sender.location(in: self.view)
+        switch sender.state {
+        case .began, .changed, .ended:
+            ibView.center = position
+            ibView.backgroundColor = UIColor.orange
+        default:
+            break
+        }
     }
-
-//    @IBAction func ibTapGesture(_ sender: Any) {
-//        ibImageView.backgroundColor = UIColor.random
-//    }
     
-//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        super.touchesBegan(touches, with: event)
-//        guard let touch = touches.first else { return }
-//        let movableViewPoint = touch.location(in: ibImageView)
-//        ibImageView.backgroundColor = .magenta
-//        if ibImageView.point(inside: movableViewPoint, with: event) {
-//            isMovableViewTouch = true
-//        }
-//    }
-//    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        super.touchesMoved(touches, with: event)
-//        guard isMovableViewTouch else { return }
-//        guard let touch = touches.first else { return }
-//        let touchPoint = touch.location(in: view)
-//        ibImageView.center = touchPoint
-//    }
-//    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        super.touchesEnded(touches, with: event)
-//        isMovableViewTouch = false
-//    }
-//    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        super.touchesCancelled(touches, with: event)
-//        ibImageView.backgroundColor = UIColor.orange
-//        isMovableViewTouch = false
-//    }
+    @objc func pinchGesture(_ sender: UIPinchGestureRecognizer) {
+        guard let transformedView = sender.view else { return }
+        transformedView.transform = transformedView.transform.scaledBy(x: sender.scale, y: sender.scale)
+        sender.scale = 1
+        ibView.backgroundColor = UIColor.orange
+    }
+    
+    @objc func rotationRecognazed(_ sender: UIRotationGestureRecognizer) {
+ 
+        guard let transformRotation = sender.view else { return }
+        transformRotation.transform = transformRotation.transform.rotated(by: sender.rotation)
+        sender.rotation = 0
+        ibView.backgroundColor = UIColor.orange
+    }
+    
+    @IBAction func ibTapGesture(_ sender: Any) {
+        ibView.backgroundColor = UIColor.random
+    }
+    
 }
